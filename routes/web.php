99a8d552821502abb9e94f80e2646ab3da4038c5
Bruno <brunoo.bordignoon@gmail.com>
2024-04-15 14:16:25 -0300

    :sparkles: feat: Configura as rotas iniciais da aplicação

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -14,9 +16,19 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth', 'verified'])->group(function () {
+	Route::prefix('/dashboard')->group(function () {
+		Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+	});
+	Route::prefix('tasks')->group(function () {
+		Route::get('/', [TaskController::class, 'index'])->name('tasks');
+		Route::get('/create', [TaskController::class, 'create'])->name('tasks.create');
+		Route::get('/{task}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
+		Route::post('/store', [TaskController::class, 'store'])->name('tasks.store');
+		Route::patch('/{task}/update', [TaskController::class, 'update'])->name('tasks.update');
+		Route::delete('/{task}/delete', [TaskController::class, 'destroy'])->name('tasks.destroy');
+	});
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
